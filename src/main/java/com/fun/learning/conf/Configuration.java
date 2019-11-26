eba73e5d03c7a3ebf774c165ba2e8145ee3ac5a2
Vikas Chaudhary <M1052611@mindtree.com>
2019-11-26 15:33:38 -0500

    initial commit

---
+++ src/main/java/com/fun/learning/conf/Configuration.java
@@ -0,0 +1,20 @@
+package com.fun.learning.conf;
+
+import lombok.Data;
+
+/**
+ * Configuration properties.
+ */
+@Data
+public class Configuration {
+
+    DataSource datasource;
+
+    @Data
+    public static final class DataSource {
+        String driverClassName;
+        String url;
+        String username;
+        String password;
+    }
+}
