2e69d440562f07007f2f11b671595faee434df5c
vikas2453 <vikas2453@gmail.com>
2019-11-26 16:41:40 -0500

    Removing the file from outer structure

--- src/test/resources/db/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS employee (
-    id int not null auto_increment primary key,
-    last_name varchar(255),
-    first_name varchar(255),
-    address varchar(255),
-    city varchar(255)
-);
